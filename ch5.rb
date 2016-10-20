e9e65c5efd81da30d10de9e99d614f09bf19085f
Jaremy Cheetwood <jaycheetwood@gmail.com>
2016-10-20 14:26:14 -0700

    new

--- ch5.rb
+++ ch5.rb
@@ -2,8 +2,18 @@
 
 my_name = 'Jaremy Cheetwood'
 my_age = 27
-my_height = 
-my_weight
-my_eyes
-my_hair
-my_teeth
+my_height = 69 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_hair = 'White'
+my_teeth = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes abd my #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+#This line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
