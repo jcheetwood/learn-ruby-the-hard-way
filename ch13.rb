1fe5b7b06929cf1a333dd5ad453b666157f182c8
Jaremy Cheetwood <jaycheetwood@gmail.com>
2016-10-20 16:06:43 -0700

    new

---
+++ ch13.rb
@@ -0,0 +1,5 @@
+first, second, third = ARGV
+
+puts "your first varialbe is: #{first}"
+puts "your second variable is #{second}"
+puts "your third variable is #{third}"
---
+++ ch9.rb
@@ -0,0 +1,14 @@
+#Here's some new strange stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days : #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here
+with the three double-quotes.
+Well be abl to type as much as we like.
+Even 3 lines if we want.
+"""
